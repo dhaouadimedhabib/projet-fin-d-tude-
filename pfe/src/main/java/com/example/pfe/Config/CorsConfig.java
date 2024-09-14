906fe9f713e594ab3dca0b8aef066ab040869a68
dhaouadimedhabib <99907834+dhaouadimedhabib@users.noreply.github.com>
2024-09-14 19:13:06 +0200

    Update CorsConfig.java

--- pfe/src/main/java/com/example/pfe/Config/CorsConfig.java
+++ pfe/src/main/java/com/example/pfe/Config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig     {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:19720", "http://10.0.2.2:8084","http://192.168.141.198:80")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
 
