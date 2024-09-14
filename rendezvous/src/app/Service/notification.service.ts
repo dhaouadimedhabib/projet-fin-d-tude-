eed1f2318410f5c89addf304848ff8d3ce83a721
dhaouadimedhabib <99907834+dhaouadimedhabib@users.noreply.github.com>
2024-09-14 22:08:09 +0200

    Update notification.service.ts

--- rendezvous/src/app/Service/notification.service.ts
+++ rendezvous/src/app/Service/notification.service.ts
@@ -7,7 +7,7 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class NotificationService {
-  private apiUrl = 'http://localhost:8084/api/notifications'; // Update this if necessary
+  private apiUrl = 'http://192.168.141.198:8084/api/notifications'; // Update this if necessary
 
   constructor(private http: HttpClient) {}
 
